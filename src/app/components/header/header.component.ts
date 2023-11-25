44bd0515191b615c1817ffb080a9cef57ace55ca
Andrew Gomes <andrewgomes1328@gmail.com>
2023-11-25 12:45:27 -0300

    add icons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faHouse, faDiagramProject, faNewspaper } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  iconHome = faHouse
+  iconProject = faDiagramProject
+  iconArticle = faNewspaper
 }
